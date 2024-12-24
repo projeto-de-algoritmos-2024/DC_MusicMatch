0a9839d060963545280a0b106ae5ab239c33ed25
GabrielSMonteiro <100171830+GabrielSMonteiro@users.noreply.github.com>
2024-12-24 03:13:45 -0300

    Atualizar o config.py

--- backend/config.py
+++ backend/config.py
@@ -2,6 +2,6 @@
 import os
 
 class SpotifyConfig:
-    CLIENT_ID = os.getenv('Seu Client', "fc4ebaa07ae441d1aeefe351977b893b")
+    CLIENT_ID = "Seu Client")
     CLIENT_SECRET = "Seu Client")
     REDIRECT_URI = "http://localhost:5000/callback"
